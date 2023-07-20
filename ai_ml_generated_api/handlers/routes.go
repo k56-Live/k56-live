53336e90817b4e8c7558f89e96204331d89d5787
k56 Live <139924674+k56-Live@users.noreply.github.com>
2023-07-20 22:44:28 +0530

    Create routes.go

---
+++ ai_ml_generated_api/handlers/routes.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	// Handle index page
+}
+
+func UsersHandler(w http.ResponseWriter, r *http.Request) {
+	// Handle users API endpoint
+}
+
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+	// Handle data API endpoint
+}
