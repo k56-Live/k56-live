9e2fadb64e9b46dffda8f06c5ac907e3eb295bba
k56 Live <139924674+k56-Live@users.noreply.github.com>
2023-07-20 22:50:38 +0530

    Create utils.go

---
+++ common/utils.go
@@ -0,0 +1,19 @@
+package common
+
+import (
+	"math/rand"
+	"time"
+)
+
+// GenerateRandomID generates a random ID of given length
+func GenerateRandomID(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+
+	return string(b)
+}
